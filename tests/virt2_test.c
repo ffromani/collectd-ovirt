fb8ade61018d6480cf94465e69bad882e9048681
Francesco Romani <fromani@redhat.com>
2016-11-09 14:59:28 +0100

    virt2: add tests for the partitioning function
    We already had tests for the building blocks of the
    partitioning function, now we can test it.

--- tests/virt2_test.c
+++ tests/virt2_test.c
@@ -22,6 +22,8 @@
  *   Florian octo Forster <octo at collectd.org>
  **/
 
+#include "virt2_test.h"
+
 #include "../src/virt2.c" /* sic */
 #include "testing.h"
 
@@ -31,7 +33,8 @@ enum {
     DATA_MAX_LEN = 4096,
 };
 
-static int read_data (const char *path, char *out, size_t len)
+static int
+read_data (const char *path, char *out, size_t len)
 {
   char working_dir[PATH_MAX];
   char full_path[PATH_MAX];
@@ -48,6 +51,18 @@ static int read_data (const char *path, char *out, size_t len)
   return 0;
 }
 
+static char *
+read_xml (const char *path)
+{
+  char buf[DATA_MAX_LEN];
+  int err = read_data (path, buf, sizeof (buf));
+  if (err)
+    return NULL;
+  DEBUG ("using XML=[\n%s\n]\n", buf);
+  return strdup (buf);
+}
+
+/* TODO: vminfo unit tests */
 
 #define TAG "virt-0"
 DEF_TEST(virt2_domain_get_tag_null_xml)
@@ -80,10 +95,7 @@ DEF_TEST(virt2_domain_get_tag_no_metadata_xml)
   memset (&vdom, 0, sizeof (vdom));
   sstrncpy (vdom.uuid, "testing", sizeof (vdom.uuid));
 
-  char *xml_str = calloc (1, DATA_MAX_LEN);
-  read_data ("minimal.xml", xml_str, DATA_MAX_LEN);
-
-  DEBUG ("using XML=[\n%s\n]\n", xml_str);
+  char *xml_str = read_xml ("minimal.xml");
 
   int err = virt2_domain_get_tag (&vdom, xml_str);
   free (xml_str);
@@ -100,10 +112,7 @@ DEF_TEST(virt2_domain_get_tag_valid_xml)
   memset (&vdom, 0, sizeof (vdom));
   sstrncpy (vdom.uuid, "testing", sizeof (vdom.uuid));
 
-  char *xml_str = calloc (1, DATA_MAX_LEN);
-  read_data ("minimal_metadata.xml", xml_str, DATA_MAX_LEN);
-
-  DEBUG ("using XML=[\n%s\n]\n", xml_str);
+  char *xml_str = read_xml ("minimal_metadata.xml");
 
   int err = virt2_domain_get_tag (&vdom, xml_str);
   free (xml_str);
@@ -247,10 +256,120 @@ DEF_TEST(virt_include_domain_matching_tags)
   return 0;
 }
 
-DEF_TEST(virt2_partition_domains)
+static int
+always_partitionable (virt2_domain_t *vdom, virt2_instance_t *inst)
 {
+  return 1;
+}
+
+DEF_TEST(virt2_partition_domains_none)
+{
+  int ret;
+  virt2_context_t ctx;
+  memset (&ctx, 0, sizeof (ctx));
+  ctx.conf.debug_partitioning = 1;
+  ctx.state.instances = 4; // random "low" number
+
+  ret = virt2_setup (&ctx);
+  EXPECT_EQ_INT (0, ret);
+
+  virt2_instance_t *inst = &(ctx.user_data[0].inst);
+  EXPECT_EQ_STR ("virt-0", inst->tag);
+
+  inst->domains_num = 0;
+
+  GArray *part = virt2_partition_domains (inst, always_partitionable);
+  EXPECT_EQ_INT (0, part->len);
+  g_array_free (part, TRUE);
+
+  ret = virt2_teardown (&ctx);
+  EXPECT_EQ_INT (0, ret);
   return 0;
 }
+
+/* we are cheating with pointers anyway, so I'm intentionally using void * */
+static void *
+alloc_domain (const char *uuid, const char *xml_file_name)
+{
+  fakeVirDomainPtr dom = calloc (1, sizeof (struct fakeVirDomain));
+  strncpy (dom->uuid, "testing", sizeof (dom->uuid));
+  dom->xml = read_xml (xml_file_name);
+  return dom;
+}
+
+static void
+free_domain (void *_dom)
+{
+  fakeVirDomainPtr dom = _dom;
+  free (dom->xml);
+  free (dom);
+}
+
+DEF_TEST(virt2_partition_domains_one_untagged)
+{
+  int ret;
+  virt2_context_t ctx;
+  memset (&ctx, 0, sizeof (ctx));
+  ctx.conf.debug_partitioning = 1;
+  ctx.state.instances = 4; // random "low" number
+
+  ret = virt2_setup (&ctx);
+  EXPECT_EQ_INT (0, ret);
+
+  virt2_instance_t *inst = &(ctx.user_data[0].inst);
+  EXPECT_EQ_STR ("virt-0", inst->tag);
+
+  inst->domains_num = 1;
+  inst->domains_all = calloc (1, sizeof (virDomainPtr));
+  inst->domains_all[0] = alloc_domain ("testing", "minimal.xml");
+
+  GArray *part = virt2_partition_domains (inst, always_partitionable);
+  EXPECT_EQ_INT (1, part->len);
+
+  void *_dom = g_array_index (part, virDomainPtr, 0);
+  fakeVirDomainPtr fake_dom = _dom;
+  EXPECT_EQ_STR ("testing", fake_dom->uuid);
+
+  g_array_free (part, TRUE);
+
+  free_domain (inst->domains_all[0]);
+  free (inst->domains_all);
+
+  ret = virt2_teardown (&ctx);
+  EXPECT_EQ_INT (0, ret);
+  return 0;
+}
+
+DEF_TEST(virt2_partition_domains_one_untagged_unpicked)
+{
+  int ret;
+  virt2_context_t ctx;
+  memset (&ctx, 0, sizeof (ctx));
+  ctx.conf.debug_partitioning = 1;
+  ctx.state.instances = 4; // random "low" number
+
+  ret = virt2_setup (&ctx);
+  EXPECT_EQ_INT (0, ret);
+
+  virt2_instance_t *inst = &(ctx.user_data[1].inst);
+  EXPECT_EQ_STR ("virt-1", inst->tag);
+
+  inst->domains_num = 1;
+  inst->domains_all = calloc (1, sizeof (virDomainPtr));
+  inst->domains_all[0] = alloc_domain ("testing", "minimal.xml");
+
+  GArray *part = virt2_partition_domains (inst, always_partitionable);
+  EXPECT_EQ_INT (0, part->len);
+  g_array_free (part, TRUE);
+
+  free_domain (inst->domains_all[0]);
+  free (inst->domains_all);
+
+  ret = virt2_teardown (&ctx);
+  EXPECT_EQ_INT (0, ret);
+  return 0;
+}
+
 #undef TAG
 
 int main (void)
@@ -265,6 +384,10 @@ int main (void)
   RUN_TEST(virt_regular_instance_skip_domain_without_tag);
   RUN_TEST(virt_default_instance_include_domain_with_unknown_tag);
   RUN_TEST(virt_regular_instance_skip_domain_with_unknown_tag);
+
+  RUN_TEST(virt2_partition_domains_none);
+  RUN_TEST(virt2_partition_domains_one_untagged);
+  RUN_TEST(virt2_partition_domains_one_untagged_unpicked);
 
   END_TEST;
 }
---
+++ tests/virt2_test.h
@@ -0,0 +1,13 @@
+
+#ifndef VIRT2_TEST_H
+#define VIRT2_TEST_H
+
+#define UUID_STRLEN 36
+typedef struct fakeVirDomain *fakeVirDomainPtr;
+struct fakeVirDomain {
+  char uuid[UUID_STRLEN + 1];
+  char *xml;
+};
+
+#endif // VIRT2_TEST_H
+
