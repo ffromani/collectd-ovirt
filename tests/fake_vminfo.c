6bed3c71b9a807a452f2731f2990221cd4e6ac26
Francesco Romani <fromani@redhat.com>
2016-11-11 12:12:25 +0100

    dist: spawn off vminfo.? again
    it is easier to work with the code -and test it if vminfo.?
    is actually a separate entity. So spawn it off again, and keep
    it like this until explicitely asked again to merge it into virt2.c

---
+++ tests/fake_vminfo.c
@@ -0,0 +1,29 @@
+
+#include "virt2_test.h"
+
+
+typedef struct VMInfo VMInfo;
+typedef struct virDomainStatsRecord virDomainStatsRecord;
+typedef virDomainStatsRecord *virDomainStatsRecordPtr;
+
+
+int
+vminfo_parse(VMInfo *vm,
+             const virDomainStatsRecordPtr record,
+             int extrainfo)
+{
+    return 0;
+}
+
+void
+vminfo_init(VMInfo *vm)
+{
+    return;
+}
+
+void
+vminfo_free(VMInfo *vm)
+{
+    return;
+}
+
